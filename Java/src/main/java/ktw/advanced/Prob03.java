e0c05df8db7d0da25a771c775e2262361329381b
BeomSeogKim <kbs4520@daum.net>
2024-03-26 23:00:58 +0900

    STUDY MAR 26

--- Java/src/main/java/ktw/advanced/Prob03.java
+++ Java/src/main/java/ktw/advanced/Prob03.java
@@ -10,6 +10,10 @@ class Prob03 {
         }
 
         int answer = 0;
+        // idea
+        // set 자료구조를 활용해 감소시켜야 하는 값을 측정 하기
+        // set에 값이 없다면 그대로 넣어주고, 만약 있다면 0이 될 때 까지 1씩 내려서 체크
+        // 0이 될 경우에는 set에 따로 넣지 않고 종료.
         Set<Integer> set = new HashSet<>();
         for (int number : map.values()) {
             while (set.contains(number) && number != 0) {
---
+++ Java/src/main/java/ktw/advanced/Prob04.java
@@ -0,0 +1,31 @@
+package ktw.advanced;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Prob04 {
+    public int solution(int[] nums, int m){
+        // sum의 값, 횟수
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int answer = 0;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - m)){
+                answer += map.get(sum - m);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Prob04 T = new Prob04();
+        System.out.println(T.solution(new int[]{2, 2, 3, -1, -1, -1, 3, 1, 1}, 5));
+        System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2, 2, -3}, 5));
+        System.out.println(T.solution(new int[]{1, 2, 3, -3, 1, 2}, 3));
+        System.out.println(T.solution(new int[]{-1, 0, 1}, 0));
+        System.out.println(T.solution(new int[]{-1, -1, -1, 1}, 0));
+    }
+}
