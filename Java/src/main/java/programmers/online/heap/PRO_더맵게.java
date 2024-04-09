aafc9b5362e597207506d3505f04043d5537d706
BeomSeogKim <kbs4520@daum.net>
2024-04-09 23:18:58 +0900

    STUDY APR 9

---
+++ Java/src/main/java/programmers/online/heap/PRO_더맵게.java
@@ -0,0 +1,29 @@
+package programmers.online.heap;
+
+import java.util.PriorityQueue;
+
+public class PRO_더맵게 {
+
+    class Solution {
+        public int solution(int[] scoville, int K) {
+            PriorityQueue<Integer> queue = new PriorityQueue<>();
+            for(int s : scoville){
+                queue.add(s);
+            }
+
+            int answer = 0;
+            int min = queue.peek();
+            while(min < K){
+                if(queue.size() >= 2){
+                    queue.add(queue.poll() + (queue.poll() * 2));
+                    min = queue.peek();
+                    answer++;
+                } else {
+                    return -1;
+                }
+            }
+            return answer;
+        }
+    }
+
+}
